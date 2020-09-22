3677222f3c5cb9aaa2158457f259d4112fabe076
WeslyG <weslyg22@gmail.com>
2020-09-22 13:29:01 +0500

    add 9.2

---
+++ src/9_2/9_2_test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from s9_2 import get_pi
+
+def test_s9_2():
+    assert get_pi() == 3.14
---
+++ src/9_2/s9_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from math import pi
+
+
+def get_pi() -> float:
+    return round(pi, 2)
