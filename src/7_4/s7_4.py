059a7c888be8c6980ed27e5202e9c9efda61389a
Vladimir Lila <lila@skbkontur.ru>
2020-09-22 18:59:43 +0500

    Add graphic

--- src/7_4/s7_4.py
+++ src/7_4/s7_4.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+
 def custom_function(x: float) -> float:
     if (x > -2.4 and x < 5.7):
         return x*x
     else:
         return 4
+
+
+# a = np.arange(-10, 10, 0.5).tolist()
+# b = []
+# for i in a:
+#     b.append(custom_function(i))
+
+# plt.plot(a, b)
+# plt.ylabel('Function value')
+# plt.xlabel('User input')
+# plt.show()
